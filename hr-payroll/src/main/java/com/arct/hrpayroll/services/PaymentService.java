ba58666052332f4a8d68932efb818ff5cf7b2cfc
ANDRERAIMUNDO <9000andre@gmail.com>
2023-01-20 12:22:33 -0300

    Implementando FeingClient

--- hr-payroll/src/main/java/com/arct/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/arct/hrpayroll/services/PaymentService.java
@@ -1,30 +1,21 @@
 package com.arct.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.arct.hrpayroll.entities.Payment;
 import com.arct.hrpayroll.entities.Work;
+import com.arct.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);
 		
-		Work worker = restTemplate.getForObject(workerHost + "/workers/{id}", Work.class, uriVariables);
+		Work worker = workerFeignClient.findId(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
