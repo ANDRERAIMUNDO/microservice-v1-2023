1dda1cf6af4e1a17b2826b89b0dec8f7d2ac87a4
ANDRERAIMUNDO <9000andre@gmail.com>
2023-01-24 13:53:01 -0300

    Configurando Hystrix

--- hr-payroll/src/main/java/com/arct/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/arct/hrpayroll/resources/PaymentResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.arct.hrpayroll.entities.Payment;
 import com.arct.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping(value = "/payments")
@@ -17,9 +18,15 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 
+	@HystrixCommand(fallbackMethod="getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = service.getPayment(workerId, days);
+		return ResponseEntity.ok(payment);
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) { 
+		Payment payment = new Payment("Example Maria", 300.00, days);
 		return ResponseEntity.ok(payment);
 	}
 }
