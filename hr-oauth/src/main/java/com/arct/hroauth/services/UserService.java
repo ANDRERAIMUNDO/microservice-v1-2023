a6bf1a00ffc07ea176ce1abc3f87d88149b04d9a
ANDRERAIMUNDO <9000andre@gmail.com>
2023-02-03 14:11:30 -0300

    login oauth jtw token

--- hr-oauth/src/main/java/com/arct/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/arct/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.arct.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.arct.hroauth.entities.User;
 import com.arct.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private static Logger log = LoggerFactory.getLogger(UserService.class);
 	
@@ -23,6 +26,17 @@ public class UserService {
 			throw new IllegalArgumentException("Email não encontrado!");
 		}
 		log.info("Email " + email + " " + "encontrado!");
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			log.error ("Email " + username + " " + "não encotrado!");
+			throw new UsernameNotFoundException("Email não encontrado!");
+		}
+		log.info("Email " + username + " " + "encontrado!");
 		return user;
 	}
 }
